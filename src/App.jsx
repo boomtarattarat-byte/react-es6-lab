b149f8e4bba8c6ae921424b72f5314646e9e7df6
Jilliane Loraine T. De Jesus <mjldejesus@tip.edu.ph>
2026-01-29 23:27:55 +0800

    Add contiditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserList from './UserList';
+import Counter from './Counter'; 
 
 function App() {
   
@@ -14,14 +15,20 @@ function App() {
   const {name, age} = person;
   console.log('Destructed:', name, age);
 
+  const isLoggedIn = true; // or false to test
+
   return (
     <div>
        <h1>React ES6 Lab</h1>
+
+       {isLoggedIn ? <p>Welcome Back!</p> : <p>Please log in</p>}
+       
        <p>Name: {name}</p>
        <p>Age: {age}</p>
 
        {/*Pass names array to UserList*/}
        <UserList users={names} />
+       <Counter /> {/* <-- Add Counter component here*/}
     </div>
   );
 }
